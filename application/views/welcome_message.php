b798bd6415aa8b178594763067429a30d4bffafa
agungfp <agungjunior541@gmail.com>
2019-06-22 20:41:35 +0700

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter With Vue Js</title>
+	<link rel="stylesheet" href="<?php echo base_url('assets/dist/app.css'); ?>">
+</head>
+<body>
+
+<div id="app"></div>
+
+	<script src="<?php echo base_url('assets/dist/app.js'); ?>"></script>
+</body>
+</html>
